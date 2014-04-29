1ae8952e69b5e1364541fe902cd2e35a868b0220
Sam Thornton <sdthornton@live.com>
2014-04-29 15:43:14 -0700

    Update UserMailer again

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,14 @@ class UserMailer < ActionMailer::Base
 
   def last_day_to_bid(user)
     @user = user
-    mail(to: @user.email, from: 'admin@azcutthechi.com',
+    email_with_name = "#{@user.name} <#{@user.email}>"
+    from_with_name = "Cut the Chi <reminder@azcutthechi.com>"
+    delivery_options = {
+      user_name: ENV['USER_MAILER_USER'],
+      password: ENV['USER_MAILER_PASS']
+    }
+    mail(to: email_with_name, from: from_with_name,
+        delivery_method_options: delivery_options,
         subject: 'Only One More Day to Bid!')
   end
 end
