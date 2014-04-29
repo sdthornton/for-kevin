79c07be20c7f18a319d0925ea074886a642e8fa9
Sam Thornton <sdthornton@live.com>
2014-04-29 15:24:57 -0700

    Simplify UserMailer.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,12 +3,11 @@ class UserMailer < ActionMailer::Base
 
   def last_day_to_bid(user)
     @user = user
-    email_with_name = "#{@user.name} <#{@user.email}>"
     delivery_options = {
       user_name: ENV['USER_MAILER_USER'],
       password: ENV['USER_MAILER_PASS']
     }
-    mail(to: email_with_name, from: 'reminder@azcutthechi.com',
+    mail(to: @user.email, from: 'reminder@azcutthechi.com',
         delivery_method_options: delivery_options,
         subject: 'Only One More Day to Bid!')
   end
